f5dff948e9cd32c09d1ce0869fa49a8ee853aaf1
Nikolay <nick@firedev.com>
2022-05-25 10:58:15 +0700

    Inifinite scroll

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @pagy, @posts = pagy_countless(Post.all, items: 5)
+    render "index_infinite" if params[:page]
   end
 
   # GET /posts/1 or /posts/1.json
